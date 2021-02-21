6a3cc8bbdf729b6a54b0533a3f61b357aa3021fe
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-21 12:11:36 -0500

    Favicon & skeleton js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-alert('hello, world!');
+const myLibrary = [1,2];
+const libraryDisplay = document.querySelector('#library');
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooksInLibrary() {
+    myLibrary.forEach(book => {
+        const p = document.createElement('p');
+        p.textContent = book;
+        libraryDisplay.appendChild(p);
+    })
+}
+
+displayBooksInLibrary();
