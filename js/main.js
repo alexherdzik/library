5e9e325daf67e5cdfb1046caf7f83a9ffd80792c
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-22 20:05:22 -0500

    Add modal window with open and close functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 const myLibrary = [1,2];
-const libraryDisplay = document.querySelector('#library');
+const libraryDisplay = document.getElementById('library');
+const newBookBtn = document.getElementById('new-book');
+const modal = document.querySelector('.modal');
+const openClass = 'open';
+
+/* Modal button */
+newBookBtn.addEventListener('click', () => {
+    document.querySelector('.modal').classList.add(openClass);
+});
+
+modal.addEventListener('click', event => {
+    if (event.target === modal) {
+        modal.classList.remove(openClass);
+    }
+});
+
+document.addEventListener('keydown', event => {
+    if (event.key === "Escape" && document.querySelector(".modal.open")) {
+        modal.classList.remove(openClass);
+    }
+});
+
+displayBooksInLibrary();
 
 function Book() {
 
@@ -16,5 +38,3 @@ function displayBooksInLibrary() {
         libraryDisplay.appendChild(p);
     })
 }
-
-displayBooksInLibrary();
