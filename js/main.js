8660d7513395e7fc7de6ef09de451148d6d28cc3
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-26 15:14:07 -0500

    Display all book properties

--- js/main.js
+++ js/main.js
@@ -37,14 +37,20 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    myLibrary.push(new Book('dummy title'));
+    const title = document.getElementById('book-title').value;
+    const author = document.getElementById('book-author').value;
+    const pages = document.getElementById('book-pages').value;
+    const read = document.getElementById('book-read').checked;
+    myLibrary.push(new Book(title, author, pages, read));
 }
 
 function displayBooksInLibrary() {
     myLibrary.forEach(book => {
-        const p = document.createElement('p');
-        p.textContent = book.title;
-        libraryDisplay.appendChild(p);
+        for (const key in book) {
+            const p = document.createElement('p');
+            p.textContent = `${key}: ${book[key]}`;
+            libraryDisplay.appendChild(p);
+        }
     })
 }
 
