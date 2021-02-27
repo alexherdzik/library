2ab0859ddd8a43b44abb4ca4a1e93928e1b3c433
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-27 10:08:55 -0500

    Improved display of books

--- js/main.js
+++ js/main.js
@@ -46,16 +46,47 @@ function addBookToLibrary() {
 
 function displayBooksInLibrary() {
     myLibrary.forEach(book => {
-        for (const key in book) {
-            const p = document.createElement('p');
-            p.textContent = `${key}: ${book[key]}`;
-            libraryDisplay.appendChild(p);
-        }
+        const bookElement = createBookElement(book);
+        libraryDisplay.appendChild(bookElement);
     })
+}
+
+function displayNewBookInLibrary() {
+
 }
 
 function clearLibraryDisplay() {
     while (libraryDisplay.firstChild) {
         libraryDisplay.removeChild(libraryDisplay.lastChild);
     }
+}
+
+function createBookElement(book) {
+    const card = document.createElement('div');
+    const cardBody = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('p');
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    read.textContent = (book.read) ? 'Read' : 'Not yet read'; 
+
+    card.classList.add('card');
+    cardBody.classList.add('card-body');
+    title.classList.add('card-title');
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(author);
+    cardBody.appendChild(pages);
+    cardBody.appendChild(read);
+    cardBody.appendChild(removeBtn);
+
+    card.appendChild(cardBody);
+
+    return card;
 }
