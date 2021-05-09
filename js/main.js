6fdcd42e5b0a93d55e3a7d367c248a10fd46dc05
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-05-09 13:49:12 -0400

    changed book to class syntax

--- js/main.js
+++ js/main.js
@@ -29,11 +29,13 @@ document.addEventListener('keydown', event => {
 
 displayBooksInLibrary();
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary() {
